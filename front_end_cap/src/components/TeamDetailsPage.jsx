d1992025d93da349830cf2e600bac234ebd607d7
Sonia Gupta <sharmilagu12@gmail.com>
2025-05-12 20:02:52 -0400

    Updated styling to look more refined

--- front_end_cap/src/components/TeamDetailsPage.jsx
+++ front_end_cap/src/components/TeamDetailsPage.jsx
@@ -3,8 +3,9 @@ import { useGetTeamDetailsQuery } from "../api/nbaAPI";
 
 const TeamDetailsPage = () => {
   const { teamName } = useParams();
-
   const { data: team, isLoading, error } = useGetTeamDetailsQuery(teamName);
+
+  console.log("Team Details:", team);
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error loading team details</p>;
@@ -13,29 +14,42 @@ const TeamDetailsPage = () => {
     <div className="team-details-container">
       {/* Display team logo and name */}
       <div className="team-header">
-      <img src={team?.teamLogo} alt={team?.teamName} 
+      <img src={team?.teamBadge} alt={team?.teamName} 
       className="team-details-logo" />
       <h2 className="team-details-name">{team?.teamName}</h2>
     </div>
 
     {/* Display team details */}
-    <div className="team-details-content">
       <div className="team-info">
         <h3>Team Information</h3>
-        <p><strong>Founded:</strong> {team?.formedYear}</p>
-        <p><strong>Stadium:</strong> {team?.stadium}</p>
-        <p><strong>Location:</strong> {team?.city}</p>
-        <p><a href={team?.website?.startsWith('http') ? team?.website : `https://${team?.website}`} 
+        <div className="team-info-details">
+          <div> 
+            <strong>Founded</strong> 
+            <p>{team?.formedYear}</p>
+          </div>
+          <div>
+            <strong>Stadium</strong>
+            <p>{team?.stadium}</p>
+          </div>
+          <div>
+            <strong>Location</strong>
+            <p>{team?.city}</p>
+          </div>
+        </div>
+        <p>
+          <a href={team?.website?.startsWith('http') ? team?.website : `https://${team?.website}`} 
         target="_blank" 
         rel="noopener noreferrer" // to prevent security issues
         >
           {team?.website}
           </a>
-          </p>
+        </p>
+      </div>
+
+      <div className="team-description">
         <p>{team?.description}</p>
       </div>
     </div>
-  </div>
   );
 };
 export default TeamDetailsPage;
